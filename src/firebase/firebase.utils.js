f7cfaa48fc277d7fb5b78da8ec5e0284fe7325cb
zagainov nikolay <zagainov_n_a@mail.ru>
2020-04-06 19:20:33 +0800

    user sign-up system with furebase added

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,32 @@ const config = {
     measurementId: "G-87S4RM0R71"
 };
 
+export const createUserProfileDocumrnt = async(userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists){
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try{
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return userRef;
+
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
--- src/pages/sighn-in-and-sighn-up/sighn-in-and-sighn-up.component.jsx
+++ src/pages/sighn-in-and-sighn-up/sighn-in-and-sighn-up.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 import './sighn-in-and-sighn-up.styles.scss';
 
 const SighnInAndUpPage = () => (
-    <div className='sighn-in-and-sighn-up'>
+    <div className='sign-in-and-sighn-up'>
         <SignIn/>
+        <SignUp/>
     </div>
 
 );
